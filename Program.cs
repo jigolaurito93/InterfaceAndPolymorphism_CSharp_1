3a3b9ef5c2d6325f31941b04e9ddc1024d29f5d4
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-19 03:44:50 -0500

    set up project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace InterfaceAndPolymorphism_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!!!");
+        }
+    }
+}
