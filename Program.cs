c177acc1e5e085f36303440d93cb52e556077a7f
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-20 02:00:17 -0500

    Creating a folder for logging a text file

--- Program.cs
+++ Program.cs
@@ -40,10 +40,24 @@ namespace InterfaceAndPolymorphism_CSharp
             // We call the ProcessOrderPayment method again, but now it will call the ProcessPayment method of the PayPalProcessor instance, demonstrating polymorphism.
             paymentService.ProcessOrderPayment(200.00m);
 
+            // The @ sign in C# is used to create a verbatim string literal, which allows us to include backslashes without needing to escape them.
+            string directoryPath = @"C:\Users";
+            string message = "This is a message that will be written to the log file.\n";
+
+            // We can combine the directory path with the file name to create a full file path using the Path.Combine method, 
+            // which ensures that the correct directory separator is used.
+            string filePath = System.IO.Path.Combine(directoryPath, "log.txt");
+
+            // If the directory specified in the directoryPath variable does not exist, we can create it using the Directory.CreateDirectory method.
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
             // Using the File class, we can write a long message to a log file called "log.txt".
             // The AppendAllText method will create the file if it doesn't exist, or append to it if it does.
-            // The first argument is the file name, and the second argument is the message we want to write, followed by a newline character for better readability.
-            File.AppendAllText("log.txt", "Hello, this is a long message that will be written to the log file.\n");
+            // The first argument is the file name or file path, and the second argument is the message we want to write, followed by a newline character for better readability.
+            File.AppendAllText(filePath, message);
         }
     }
 }
--- obj/Debug/net8.0/InterfaceAndPolymorphism_CSharp_1.AssemblyInfo.cs
+++ obj/Debug/net8.0/InterfaceAndPolymorphism_CSharp_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterfaceAndPolymorphism_CSharp_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56a53a771539a3798a7ec841f0ed4ddc920fe74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a96a43ef233f924dd680515b9754ba4898ba4bba")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterfaceAndPolymorphism_CSharp_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterfaceAndPolymorphism_CSharp_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
