a56a53a771539a3798a7ec841f0ed4ddc920fe74
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-20 00:53:51 -0500

    Adding comments for clarity

--- PaymentService.cs
+++ PaymentService.cs
@@ -3,11 +3,18 @@ namespace InterfaceAndPolymorphism_CSharp
 {
     public class PaymentService
     {
+        // The PaymentService class depends on the IPaymentProcessor interface, allowing it to process payments without being tightly coupled to a specific payment method. 
+        // This design promotes flexibility and adherence to the Dependency Inversion Principle.
         private readonly IPaymentProcessor _processor;
+
+        // The constructor of the PaymentService class takes an IPaymentProcessor as a parameter, enabling dependency injection.
         public PaymentService(IPaymentProcessor processor)
         {
             _processor = processor;
         }
+
+        // The ProcessOrderPayment method uses the injected IPaymentProcessor to process the payment for an order, 
+        // demonstrating polymorphism as it can work with any implementation of the IPaymentProcessor interface.
         public void ProcessOrderPayment(decimal amount)
         {
             _processor.ProcessPayment(amount);
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace InterfaceAndPolymorphism_CSharp
     {
         static void Main(string[] args)
         {
+            /*
             Dog bulldog = new Dog();
             bulldog.Speak();
             bulldog.Eat("bone");
@@ -12,13 +13,31 @@ namespace InterfaceAndPolymorphism_CSharp
             Cat siamese = new Cat();
             siamese.Speak();
             siamese.Eat("fish");
+            */
 
+            // Demonstrating polymorphism with the PaymentService and IPaymentProcessor interface
+
+            // We create an instance of CreditCardProcessor that has a method called ProcessPayment()
+            // which takes a decimal amount and prints a message.
+            // We use IPaymentProcessor interface as a reference type.
             IPaymentProcessor creditCardProcessor = new CreditCardProcessor();
+
+            // We create an instance of PaymentService class that takes an IPaymentProcessor type in its constructor.
+            // We passed the creditCardProcessor instance/object to the PaymentService as an argument.
             PaymentService paymentService = new PaymentService(creditCardProcessor);
+
+            // We call the ProcessOrderPayment method from the PaymentService class, 
+            // which internally calls the ProcessPayment method of the CreditCardProcessor instance.
             paymentService.ProcessOrderPayment(150.00m);
 
+            // We create an instance of PayPalProcessor that also implements the IPaymentProcessor interface and has its own implementation of the ProcessPayment() method
+            // which takes a decimal amount and prints a message specific to PayPal payments.
             IPaymentProcessor paypalProcessor = new PayPalProcessor();
+
+            // We create another instance of PaymentService class, but this time we pass the paypalProcessor instance/object to it.
             paymentService = new PaymentService(paypalProcessor);
+
+            // We call the ProcessOrderPayment method again, but now it will call the ProcessPayment method of the PayPalProcessor instance, demonstrating polymorphism.
             paymentService.ProcessOrderPayment(200.00m);
 
         }
